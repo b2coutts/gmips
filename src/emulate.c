bdd0465fe772b682f6484d40cf1e73b5ed1d3fd3
Bryan Coutts <b2coutts@uwaterloo.ca>
2014-01-13 16:50:21 -0500

    Made emulate read from file instead of STDIN

--- src/emulate.c
+++ src/emulate.c
@@ -10,12 +10,23 @@
 
 typedef unsigned char byte;
 
-int main(){
+int main(int argc, char *argv[]){
+    if(argc <= 1){
+        fprintf(stderr, "Usage: %s [mips_binary]\n", argv[0]);
+        return 1;
+    }
+
+    FILE *fp = fopen(argv[1], "r");
+    if(!fp){
+        fprintf(stderr, "Could not open file \"%s\".\n", argv[1]);
+        return 2;
+    }
+
     struct machine m = machine_create(N_BYTES);
 
     // read code into memory; TODO: binary stream functions?
     int b;
-    for(int i = 0; (b = getchar()) != -1; i++){
+    for(int i = 0; (b = fgetc(fp)) != EOF; i++){
         m.mem[i] = b;
     }
 
